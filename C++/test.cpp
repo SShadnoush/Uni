69891d1ea65f03919edc008abe87dcc703ee47fb
SShadnoush <123301412+SShadnoush@users.noreply.github.com>
2023-10-29 22:38:30 +0330

    Add files via upload

---
+++ C++/test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <conio.h>
+    //iostream for cou - conio.h for getch 
+
+using namespace std;
+    //instead of this line we can use std:: 
+
+main()
+{
+    int s=37000;
+    cout<<"this code had written by Zahra Shadnoush";
+    cout<<endl<<"\ns is:"<<s;
+    cout<<"\n";
+    double d=987654;
+    cout<<endl<<"d is:"<<d; 
+    cout<<endl;
+    float f=46.73;
+    cout<<"\nf is :"<<f << endl;
+    char c='s';
+    cout<<endl<<"c is:"<<c;
+    getch();
+    //Hello world in double qoutation --> string
+    //s --> integer
+}
